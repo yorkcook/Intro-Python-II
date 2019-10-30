1bba183d32ea4a7c8bea99a1963670a76ea2cf1d
York Cook <yorkcook@Johnny-Castle.local>
2019-10-30 17:56:29 -0400

    WIP: Initial Commit

--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = ""
+        self.e_to = ""
+        self.s_to = ""
+        self.w_to = ""
